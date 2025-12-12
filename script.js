d6e0c093b160791347e220e0bf334383d1bf9ff4
Hasnain <hasnainshams7@gmail.com>
2025-12-12 10:23:46 +0500

    I have added a logic for register in js file

--- script.js
+++ script.js
@@ -2,6 +2,28 @@
 let parked = JSON.parse(localStorage.getItem("parked")) || [];
 let history = JSON.parse(localStorage.getItem("history")) || [];
 
+
+// --- Register Vehicle ---
+function registerVehicle() {
+    let vnum = document.getElementById("vnum").value;
+    let owner = document.getElementById("owner").value;
+    let type = document.getElementById("type").value;
+    let slot = document.getElementById("slot").value;
+
+    let vehicle = {
+        vnum,
+        owner,
+        type,
+        slot,
+        timeIn: new Date().toLocaleString()
+    };
+
+    parked.push(vehicle);
+    localStorage.setItem("parked", JSON.stringify(parked));
+
+    alert("Vehicle Registered Successfully!");
+    window.location.href = "parked.html";
+}
 // --- Show Parked Vehicles ---
 if (document.getElementById("parkedList")) {
     let list = "";
